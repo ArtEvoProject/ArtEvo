ece17456eb5626f3ee3de3fd4b20a5a9ed7e0559
ishainamdar <ishainamdar2004@gmail.com>
2025-11-19 16:07:03 +0530

    new

---
+++ Art_Evo/src/main/java/com/art/Application.java
@@ -0,0 +1,26 @@
+package com.art;
+
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	ModelMapper modelMapper() {
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration() //get default config
+		.setPropertyCondition(Conditions.isNotNull()) 
+		.setMatchingStrategy(MatchingStrategies.STRICT);
+		return mapper;
+	}
+
+}
